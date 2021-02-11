61537d6427e0ce79ac6755c768a3a3766e048dd9
lancetipton <lancetipton04@gmail.com>
2021-02-11 10:17:54 -0700

    fix(eslint): Updated eslint config

--- configs/eslintrc.config.js
+++ configs/eslintrc.config.js
@@ -5,29 +5,21 @@ module.exports = {
     jest: true,
     __DEV__: true,
     expect: true,
-    React: true,
   },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: {},
   },
   env: {
     es6: true,
     browser: true,
     node: true,
     'jest/globals': true,
-    'react-native/react-native': true,
   },
-  plugins: [ 'jest', 'react', 'react-native' ],
-  extends: [ 'plugin:jest/recommended', 'plugin:react/recommended' ],
-  settings: {
-    react: {
-      version: '16.9',
-    },
-  },
+  plugins: [ 'jest' ],
+  extends: [ 'plugin:jest/recommended' ],
+  settings: {},
   rules: {
     /* General */
     'no-console': [ 'warn', { allow: [ 'warn', 'error' ] }],
@@ -89,34 +81,6 @@ module.exports = {
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-    /* React-Native */
-    'react-native/no-raw-text': 1,
-    'react-native/no-unused-styles': 2,
-    'react-native/split-platform-components': 2,
-    /* React */
-    'react/jsx-curly-spacing': [
-      2,
-      {
-        when: 'always',
-        attributes: false,
-        children: true,
-        spacing: {
-          objectLiterals: 'never',
-        },
-      },
-    ],
-    'react/prop-types': 0,
-    'react/display-name': 0,
-    'react/no-children-prop': 0,
-    'react/jsx-first-prop-new-line': [ 'error', 'multiline' ],
-    'react/jsx-max-props-per-line': [
-      'error',
-      {
-        maximum: 1,
-        when: 'always',
-      },
-    ],
-    'react/jsx-closing-bracket-location': [ 'error', 'line-aligned' ],
+    'jest/valid-expect': 'error'
   },
 }
