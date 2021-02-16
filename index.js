2b0e8262988cd5e7d1c5ffc691e298fd6f3b9279
lancetipton <lancetipton04@gmail.com>
2021-02-16 03:22:00 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,41 @@
+const $ = selector => document.querySelector(selector)
+const world = {}
+
+let PK
+let debug = true
+
+const logDebug = (...data) => {
+  debug && console.log(...data)
+}
+
+const testFeature = () => {
+  const featureText = $(`#feature-text`).value
+  const parsedFeature = PK.parse.feature(featureText)
+  logDebug(`Parsed feature:\n`, parsedFeature)
+
+  PK.run(parsedFeature)
+
+}
+
+const setupDefinitions = () => {
+  // Remove any pre-loaded steps
+  PK.steps.clear()
+
+  // Re-parse the definition text
+  const definitionText = $(`#definition-text`).value
+  const parsedDefinition = PK.parse.definition(definitionText)
+
+  logDebug(`Parsed definitions:\n`, parsedDefinition)
+
+  // Register the parsed definitions
+  PK.steps.register(parsedDefinition)
+}
+
+const runFeature = () => {
+  setupDefinitions()
+  testFeature()
+}
+
+(() => {
+  PK = new Parkin()
+})()
