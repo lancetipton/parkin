fe1315ddf9f84daf0e36c2281de751bc438369a9
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-19 09:12:45 -0700

    added unit test and mocks

--- src/__tests__/runner.js
+++ src/__tests__/runner.js
@@ -1,7 +1,5 @@
-import { feature, registerMockSteps } from '../__mocks__'
+import { feature, registerMockSteps, mockHooks } from '../__mocks__'
 import { constants } from '../constants'
-
-const { REGEX_VARIANT, EXPRESSION_VARIANT } = constants
 
 jest.resetModules()
 jest.resetAllMocks()
@@ -11,14 +9,40 @@ const worldObj = {}
 const { Parkin } = require('../parkin')
 
 describe('Runner', () => {
-  it('should handel features as a string', async () => {
+
+  let ogTest;
+  let resetHooks;
+  beforeEach(() => {
+    ogTest = global.test
+    global.test = () => {}
+    resetHooks = mockHooks()
+  })
+  afterEach(() => {
+    global.test = ogTest
+    resetHooks()
+  })
+
+  it('should handle features as a string', async () => {
     const PK = new Parkin(worldObj)
     registerMockSteps(PK)
 
-    const ogTest = global.test
-    global.test = () => {}
     const resp = await PK.run(feature)
     expect(resp).toBe(true)
-    global.test = ogTest
+  })
+
+  it ('should call hooks', async () => {
+    const PK = new Parkin(worldObj)
+    registerMockSteps(PK)
+
+    const testBefore = jest.fn()
+    const testAfter = jest.fn()
+    PK.hooks.beforeAll(testBefore)
+    PK.hooks.afterAll(testAfter)
+
+    const resp = await PK.run(feature)
+    expect(resp).toBe(true)    
+
+    expect(testBefore).toHaveBeenCalled()
+    expect(testAfter).toHaveBeenCalled()
   })
 })
