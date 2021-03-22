aa4e31ecfd5f139500eb0f78d5627529cd687ec4
Lance Tipton <lancetipton04@gmail.com>
2021-03-22 10:58:09 -0700

    Updates

--- jest.js
+++ jest.js
@@ -16,6 +16,19 @@
 
   window.test = (message, method) => {
     console.log(message)
+    isFunc(method) && method(() => {})
+  }
+
+  window.beforeAll = (method) => {
+    isFunc(method) && method()
+  }
+  window.afterAll = (method) => {
+    isFunc(method) && method()
+  }
+  window.beforeEach = (method) => {
+    isFunc(method) && method()
+  }
+  window.afterEach = (method) => {
     isFunc(method) && method()
   }
 
