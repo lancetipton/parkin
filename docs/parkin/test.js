7f3d63368eb02077509b3a19c7fac18145b02ade
Lance Tipton <lancetipton04@gmail.com>
2022-05-29 14:42:21 -0700

    Updating tap to version 1.2.1

--- docs/parkin/test.js
+++ docs/parkin/test.js
@@ -1,13 +1,1 @@
-System.register(['./test-8531a9fa.js', './wait-26c5e7c6-2112d3c7.js'], (function (exports) {
-	'use strict';
-	return {
-		setters: [function (module) {
-			exports('ParkinTest', module.P);
-		}, function () {}],
-		execute: (function () {
-
-
-
-		})
-	};
-}));
+System.register(["./test-8531a9fa.js","./wait-26c5e7c6-2112d3c7.js"],(function(t){"use strict";return{setters:[function(e){t("ParkinTest",e.P)},function(){}],execute:function(){}}}));
