d8268fa8ba8a601cf2ac135ba6e178d1c62c989b
Lance Tipton <lancetipton04@gmail.com>
2023-02-04 17:06:12 -0700

    chore:build with updates

--- docs/parkin/test.js
+++
@@ -1,1 +0,0 @@
-System.register(["./test-354f8679.js","./wait-26c5e7c6-fdf32dc0.js"],(function(t){"use strict";return{setters:[function(e){t("ParkinTest",e.P)},function(){}],execute:function(){}}}));
