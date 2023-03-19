8c3fb489d34a01eca08b4b8441f660feabb5c809
Lance Tipton <lancetipton04@gmail.com>
2023-03-19 16:58:56 -0700

    fix: export to indexes

--- src/assemble/assemble.ts
+++ src/assemble/assemble.ts
@@ -1,7 +1,9 @@
 import { assembleFeature as feature } from './feature'
 import { fromIndex } from './fromIndex'
+import { featureToIndexes as toIndexes } from './featureToIndexes'
 
 export const assemble = {
   feature,
+  toIndexes,
   fromIndex,
 }
